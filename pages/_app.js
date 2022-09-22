c6828a0afc992146d5008934b6b5f7e89633210d
Stanly Calle <callestanly@gmail.com>
2022-09-22 00:18:27 -0500

    fixed build error

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
-import createEmotionCache from "./src/createEmotionCache";
-import theme from "./src/theme";
+import createEmotionCache from "../src/createEmotionCache";
+import theme from "../src/theme";
 import "../styles/globals.css";
 import Head from "next/head";
 
