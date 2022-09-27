ac46bf95aefbd414878161dc02a7474143042020
Stanly Calle <callestanly@gmail.com>
2022-09-27 00:22:46 -0500

    Update Card.js

--- components/Products/Card/Card.js
+++ components/Products/Card/Card.js
@@ -6,35 +6,52 @@ import { convertToPath } from "../../../lib/utils";
 import styles from "./Card.module.css";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import shoppingContext from "../../../src/context/shoppingContext";
+import AddToCartModal from "../../ShoppingCart/AddToCartModal";
+import modalContext from "../../../src/context/modalContext";
 
 const Card = ({ item }) => {
+  console.log(item);
+  const { open, handleClose, handleOpen } = useContext(modalContext);
   const { addToCart } = useContext(shoppingContext);
+  
 
+  const handleButton = (item) => {
+    addToCart(item.id);
+    handleOpen();
+  };
 
   return (
-    <div
-      key={item.id}
-      className={styles.latestItems}>
-      <Link href={`/store/${convertToPath(item.name)}`}>
-        <a>
-          <Image
-            width={150}
-            height={150}
-            objectFit="contain"
-            src={item.img}
-            alt={item.alt}
-          />
-          <h3 className={styles.name}>{item.name}</h3>
-          <h4 className={styles.price}>{`$ ${item.price}`}</h4>
-        </a>
-      </Link>
-      <Button
-        onClick={() =>  addToCart(item.id)}
-        variant="outlined"
-        endIcon={<AddShoppingCartIcon />}>
-        ADD
-      </Button>
-    </div>
+    <>
+      <AddToCartModal
+        handleClose={handleClose}
+        open={open}
+        item={item}
+      />
+
+      <div
+        key={item.id}
+        className={styles.latestItems}>
+        <Link href={`/store/${convertToPath(item.name)}`}>
+          <a>
+            <Image
+              width={150}
+              height={150}
+              objectFit="contain"
+              src={item.img}
+              alt={item.alt}
+            />
+            <h3 className={styles.name}>{item.name}</h3>
+            <h4 className={styles.price}>{`$ ${item.price}`}</h4>
+          </a>
+        </Link>
+        <Button
+          onClick={() => handleButton(item)}
+          variant="outlined"
+          endIcon={<AddShoppingCartIcon />}>
+          ADD
+        </Button>
+      </div>
+    </>
   );
 };
 
