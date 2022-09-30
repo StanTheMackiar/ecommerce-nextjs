a2c77b6e22e12a8ded4453b00516bcade1ee413e
Stanly Calle <callestanly@gmail.com>
2022-09-30 02:46:20 -0500

    fixed image bug

--- components/Products/Card/Card.js
+++ components/Products/Card/Card.js
@@ -7,6 +7,7 @@ import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import { useContext, useState } from "react";
 import AddToCartModal from "../../Modal/AddToCartModal";
 import shoppingContext from "../../../src/context/shoppingContext";
+import newTag from "../../../public/img/new.png";
 
 const Card = ({ item, tag = false }) => {
   const { addToCart } = useContext(shoppingContext);
@@ -37,7 +38,7 @@ const Card = ({ item, tag = false }) => {
                   width={35}
                   height={35}
                   alt="new tag"
-                  src="/../public/img/new.png"
+                  src={newTag}
                 />
               </div>
             )}
--- pages/index.js
+++ pages/index.js
@@ -3,45 +3,68 @@ import { getLatestItems } from "../services/itemServices";
 import styles from "../styles/Home.module.css";
 import Card from "../components/Products/Card/Card";
 import { Button } from "@mui/material";
-import Image from "next/image";
+import Image from "next/image"
+import gif from "../public/img/gif.gif"
+
 
 export default function Home({ products }) {
-
-
   return (
     <Layout title="Welcome">
-      <h1 className={styles.title}>Welcome to the best <strong>Sport Store!</strong></h1>
+      <h1 className={styles.title}>
+        Welcome to the best <strong>Sport Store!</strong>
+      </h1>
+
       <section className={styles.banner}>
-          <div className={styles.bannerInfo}>
-            <h2>Welcome to Sports Store!</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div className={styles.bannerInfo}>
+          <h2>Welcome to Sports Store!</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
         </div>
       </section>
+
       <section className={styles.container}>
-      <div className={styles.latestSection}>
-        <h2>New products</h2>
-        <div className={styles.latestGrid}>
-          {products &&
-            products.map((item) => (
-              <Card item={item} key={item.name} tag={true}/>
-            ))}
+        <div className={styles.latestSection}>
+          <h2>New products</h2>
+          <div className={styles.latestGrid}>
+            {products &&
+              products.map((item) => (
+                <Card
+                  item={item}
+                  key={item.name}
+                  tag={true}
+                />
+              ))}
+          </div>
+
+          <Button
+            sx={{ m: "1.2rem" }}
+            variant="contained"
+            disableElevation>
+            Show more
+          </Button>
         </div>
-        <Button sx={{m: "1.2rem"}} variant="contained" disableElevation>Show more</Button>
-      </div>
-      <div className={styles.imageSection}>
-        <h2 className={styles.subtitle}>There are no limits!</h2>
-        <div className={styles.image}>
-        <Image alt="No limits gif" width={350} height={400} src="/../public/img/gif.gif" objectFit="contain"/>
+
+        <div className={styles.imageSection}>
+          <h2 className={styles.subtitle}>There are no limits!</h2>
+          <div className={styles.image}>
+            <Image
+              alt="No limits gif"
+              width={350}
+              height={400}
+              src={gif}
+              objectFit="contain"
+            />
+          </div>
         </div>
-      </div>
       </section>
     </Layout>
   );
 }
 
 export async function getStaticProps(context) {
-
-  const products = await getLatestItems()
+  const products = await getLatestItems();
 
   return {
     props: {
